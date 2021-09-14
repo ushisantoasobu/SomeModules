6b72508955278b734e54f7142ba7ab4332601a7a
ushisantoasobu <babblemann.shunsee@gmail.com>
2021-09-14 18:34:45 +0900

    add sampleApp

---
+++ sampleApp/sampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sampleApp
+//
+//  Created by 佐藤俊輔 on 2021/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
