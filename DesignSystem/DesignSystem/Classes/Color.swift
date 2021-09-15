e88d650fd7316be7cb10ae91f9e895703bedf9d4
ushisantoasobu <babblemann.shunsee@gmail.com>
2021-09-15 15:34:10 +0900

    add designsystem

---
+++ DesignSystem/DesignSystem/Classes/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  DesignSystem
+//
+//  Created by 佐藤俊輔 on 2021/09/15.
+//
+
+import Foundation
+import UIKit
+
+public struct Color {
+    public static var primary = UIColor(named: "primary")
+}
